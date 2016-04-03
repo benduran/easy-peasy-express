2bf33e48cd49e8a3c7638022dba91ac8b15bceb8
Michael Brandenburg <mcbrande@gmail.com>
2016-04-03 09:46:54 -0700

    redirect testing

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var request = require('supertest'),
-app = require('../example');
+    app = require('../example'),
+    chai = require('chai');
 
 describe('create routes', function () {
   describe('/login', function () {
@@ -26,7 +27,7 @@ describe('create routes', function () {
     });
   });
   describe('/test', function () {
-    it('should GET', function (done) {
+    it('should GET & redirects to /login?returnUrl=%2Ftest', function (done) {
       request(app).get('/test')
         .expect('Location', '/login?returnUrl=%2Ftest')
         .expect(302, done);
@@ -45,6 +46,52 @@ describe('create routes', function () {
     it("shouldn't DELETE", function (done) {
       request(app).delete('/test')
         .expect(404, done);
+    });
+  });
+
+  describe('/redirectPlease', function () {
+    it('should GET & does not redirect to /login?returnUrl=%2Ftest', function (done) {
+      request(app).get('/redirectPlease')
+        .expect("Login")
+        .expect(200, done);
+    });
+
+    it("shouldn't POST", function (done) {
+      request(app).post('/redirectPlease')
+        .expect(404, done);
+    });
+
+    it("shouldn't PUT", function (done) {
+      request(app).put('/redirectPlease')
+        .expect(404, done);
+    });
+
+    it("shouldn't DELETE", function (done) {
+      request(app).delete('/redirectPlease')
+        .expect(404, done);
+    });
+  });
+});
+
+describe('redirects', function () {
+  describe('Redirects', function () {
+    it('should GET /test & redirects to /login?returnUrl=%2Ftest', function (done) {
+      request(app).get('/test')
+        .expect(function (res) {
+          chai.expect(res.headers.location).to.be.equal('/login?returnUrl=%2Ftest');
+        })
+        .expect(302, done);
+    });
+  });
+
+  describe('Redirects & Keeps URL', function () {
+    it('should GET & /redirectPlease does not redirect to /login?returnUrl=%2Ftest', function (done) {
+      request(app).get('/redirectPlease')
+        .expect("Login")
+        .expect(function (res) {
+          chai.expect(res.headers.location).to.be.undefined;
+        })
+        .expect(200, done);
     });
   });
 });
